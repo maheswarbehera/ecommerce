7cd46181852668bea59796fd252c52aaf7e491d7
maheswar behera <bmaheswar439@gmail.com>
2025-04-26 20:03:02 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.static("public"));
 app.use(userAgent.express());
 app.use(requestLogger);
 app.use(cors({ 
-  origin: '*', 
+  origin: envConfig.CORS_ORIGIN, 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'] 

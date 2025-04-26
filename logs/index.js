9f31d6fcade0646636e2bc1c8fa18ecf50db9633
maheswar behera <bmaheswar439@gmail.com>
2025-04-26 19:54:31 +0530

    import missing

--- logs/index.js
+++ logs/index.js
@@ -2,6 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import readline from 'readline';
 import { fileURLToPath } from 'url'; 
+import sharedUtils from '../src/utils/index.js'; 
+import { LoginHistory } from '../src/models/user/login.model.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,7 +11,6 @@ const __dirname = path.dirname(__filename);
 const logFile = process.env.NODE_ENV === 'development' ? 'development.server.log' : 'production.server.log';
 
 const logPath = path.resolve(__dirname, logFile);
-console.log(logPath);
 
 function readLastLines(filePath, lineCount = 50) {
   return new Promise((resolve, reject) => {
@@ -32,6 +33,15 @@ function readLastLines(filePath, lineCount = 50) {
 }
 
 // Run script
-readLastLines(logPath, 50)
-  .then(console.log)
-  .catch(console.error);
+// readLastLines(logPath, 50)
+//   .then(console.log)
+//   .catch(console.error);
+
+const loginHistory = sharedUtils.asyncHandler(async (req, res, next) => {
+  const users = await LoginHistory.find().populate("user").sort({ lastLogin: -1 }).limit(50);
+  if (!users) return sharedUtils.ApiErrorResponse(404, "No user found", next);
+  sharedUtils.ApiSuccessResponse(res, 200, users, "Login history fetched successfully");
+
+})
+
+export {loginHistory};
